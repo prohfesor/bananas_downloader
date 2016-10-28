8171e34c8a65d44bde257af03894b601bc8a245e
Pavel Dvoinos <pavel.dvoinos@transferwise.com>
2016-10-28 12:44:14 +0300

    bot and configuration

---
+++ telegram_bot/bananas_bot.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * User: pavel
+ * Date: 28.10.16
+ * Time: 12:32
+ */
+
+require_once ("config.php");
---
+++ telegram_bot/config.sample.php
@@ -0,0 +1,6 @@
+<?php
+//
+// Telegram Bot configuration
+//
+
+$bot_token = "666666666:XXXXX-xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
