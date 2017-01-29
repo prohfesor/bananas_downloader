e33ad4296bbf0e8fb062e5f1faffd53845907469
Pavel Dvoinos <prohfesor@gmail.com>
2017-01-29 14:28:49 +0200

    send help messages

--- telegram_bot/bananas_bot.php
+++ telegram_bot/bananas_bot.php
@@ -5,10 +5,14 @@
  * Time: 12:32
  */
 
-require_once ("config.php");
-require_once ("api_functions.php");
+require_once "config.php";
+require_once "api_functions.php";
+require_once "process_functions.php";
 
 //get last updates
 $updates = get_updates();
 
 var_dump($updates);
+
+//process updates
+process($updates);
