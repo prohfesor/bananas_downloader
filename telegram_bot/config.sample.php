327586d269535190a4de1e08dd6bb5c9d33abbb5
Pavel Dvoinos <prohfesor@gmail.com>
2017-01-29 23:01:17 +0200

    changes in config

--- telegram_bot/config.sample.php
+++ telegram_bot/config.sample.php
@@ -3,8 +3,11 @@
 // Telegram Bot configuration
 //
 
-$bot_token = "666666666:XXXXX-xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+define('BOT_TOKEN', "666666666:XXXXX-xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 
-$data_dir = realpath( dirname( __FILE__ )."/../" );
-$filename_last_processed = $data_dir ."/". 'last_processed';
-$filename_downloads = $data_dir ."/". "downloads";
+define('DATA_DIR', realpath( dirname( __FILE__ )."/../") );
+define('FILENAME_LAST_PROCESSED', DATA_DIR.'/last_processed');
+define('FILENAME_DOWNLOADS', DATA_DIR.'/downloads');
+
+define('RETRY', 20);
+define('TIMEOUT_POLING', 40);
